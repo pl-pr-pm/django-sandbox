4e922860701a036282d41cbfe0ab60f44f259f23
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-01-18 16:10:54 +0900

    Views, ルーティングの設定

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,1 +1,29 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from snippets.views import top
+from django.urls import resolve
+from snippets.views import top, snippet_new, snippet_edit, snippet_detail
+
+# class TopPageTest(TestCase):
+#   def test_top_returns_200(self):
+#     response = self.client.get('/')
+#     self.assertEqual(response.status_code, 200)
+  
+#   def test_top_returns_expected_content(self):
+#     response = self.client.get('/')
+#     self.assertEqual(response.content, b"Hello World")
+
+class CreateSnippetTestCase(TestCase):
+  def test_should_resolve_snippet_new(self):
+    found = resolve("/snippets/new/")
+    self.assertEqual(snippet_new ,found.func)
+
+class SnippetDetailTestCase(TestCase):
+  def test_should_resolve_snippet_detail(self):
+    found = resolve("/snippets/1/")
+    self.assertEqual(snippet_detail ,found.func)
+
+class EditSnippetTestCase(TestCase):
+  def test_should_resolve_snippet_edit(self):
+    found = resolve("/snippets/1/edit/")
+    self.assertEqual(snippet_edit ,found.func)
